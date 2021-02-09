93f75b7aa8aaab4155b2ec0fd6c2f6bbe406117e
Christopher Dajer <chrisdajer@gmail.com>
2021-02-09 21:40:18 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,17 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  final_resuts = data.each_with_objects({}) do |(key, value), fianl_array|
+    value.each do |inner_key, names|
+      names.each do |name|
+        if !final_array[name]
+          final_array[name] = {}
+        end 
+        if !final_array[name][key]
+          !final_array[name][key] = []
+    end 
+    fianl_array [name][key].push(inner_key)
+end
+end 
+end
 end
